a493ecf01f78c8a6f7f80cf5c978ecaebbbe92e5
Rachel Sutton <raesak@gmail.com>
2025-03-12 13:14:58 -0700

    add queries and mutations

---
+++ client/src/utils/queries.ts
@@ -0,0 +1,56 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_USER = gql`
+    query user($username: String!) {
+        user(username: $username) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }`;
+export const QUERY_BOOKS = gql`
+    query getBooks {
+        books {
+            bookId
+            authors
+            description
+            title
+        }
+    }
+        `;
+    
+    export const QUERY_SINGLE_BOOK = gql`
+    query getSingleBook($bookId: ID!) {
+        book(bookId: $bookId) {
+            bookId
+            authors
+            description
+            title
+        }
+    }
+        `;
+
+export const QUERY_ME = gql`
+    query me {
+        me {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+}
+}
+`;
--- server/src/server.ts
+++ server/src/server.ts
@@ -30,7 +30,7 @@ const startApolloServer = async () => {
     });
   }
   
-  db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+  await db();
 
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
